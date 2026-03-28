bff5e8a764b455f3eebff5b4f3103f3b2fbd70e0
Titomi Oni <onititomi@gmail.com>
2026-03-27 20:59:34 -0600

    square root project

---
+++ a1.py
@@ -0,0 +1,10 @@
+import math
+
+# This when im going to ask the user for a number
+num = float(input("Enter a number: "))
+
+# Caculating the answer/ square root
+result = math.sqrt(num)
+
+# The result
+print("The square root is:", result)
